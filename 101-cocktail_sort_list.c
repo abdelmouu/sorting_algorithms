bb5c0da26e21bf00a7e1ebccd84b42442059abb0
abdallah moumni <abdelrebel001@gmail.com>
2024-02-20 08:05:49 -0800

    this commit is for a function that sorts a doubly linked list of integers in ascending order using the Cocktail shaker sort algorithm

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,72 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ *cocktail_sort_list - Sorts a doubly linked list
+ * of integers in ascending order using the 
+ * Cocktail Shaker sort algorithm.
+ *
+ *@list: list
+ */
+ 
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *node;
+	int swap_done = 1;
+
+	if (list == '\0' || (*list) == '\0' || (*list)->next == '\0')
+		return;
+	node = *list;
+	while (swap_done == 1)
+	{
+		swap_done = 0;
+		while (node->next)
+		{
+			if (node->n > node->next->n)
+			{
+				node = swap_node(node->next, list);
+				swap_done = 1;
+				print_list(*list);
+			}
+			node = node->next;
+		}
+		if (swap_done == 0)
+			break;
+		swap_done = 0;
+		while (node->prev)
+		{
+			if (node->n < node->prev->n)
+			{
+				node = swap_node(node, list);
+				swap_done = 1;
+				print_list(*list);
+			}
+			else
+				node = node->prev;
+		}
+	}
+}
+
+/**
+ *swap_node - Swap two nodes of a doubly linked list
+ *@node: The node to swap with the next node
+ *@list: The double linked lists that contains the nodes
+ *
+ *Return: return a pointer to a node which was enter it
+ */
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
+}
